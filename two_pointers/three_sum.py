763a708c235106972b6680f57f062b3cd4d453c5
ray chiu <msraychiu@gmail.com>
2022-08-03 22:09:45 +0800

    3 sum

---
+++ two_pointers/three_sum.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+# class Solution:
+def threeSum(self, nums: List[int]) -> List[List[int]]:
+    nums.sort()
+    result = []
+    for index, num in enumerate(nums):
+        if num == nums[index-1]:
+            continue
+        i, j = index+1, len(nums)-1
+        while i < j:
+            if nums[index] + nums[i] + nums[j] == 0:
+                result.append([nums[index], nums[i], nums[j]])
+                i += 1
+                while nums[i] == nums[i-1] and i < j:
+                    i += 1
+            elif nums[index] + nums[i] + nums[j] > 0:
+                j -= 1
+            else:
+                i += 1
+    return result
