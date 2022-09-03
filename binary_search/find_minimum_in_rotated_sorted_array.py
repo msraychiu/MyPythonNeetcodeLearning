c0a73b331fb89aced9db44640325eaf79c88f6db
ray chiu <msraychiu@gmail.com>
2022-09-03 21:19:30 +0800

    clean up

--- binary_search/find_minimum_in_rotated_sorted_array.py
+++ binary_search/find_minimum_in_rotated_sorted_array.py
@@ -1,23 +1,17 @@
-# class Solution:
-from typing import List
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left, right = 0, len(nums)-1
 
+        result = nums[left]
+        while left <= right:
+            if nums[right] > nums[left]:
+                result = min(result, nums[left])
+                break
+            mid = (left + right) // 2
+            result = min(result, nums[mid])
+            if nums[mid] >= nums[left]:
+                left = mid + 1
+            else:
+                right = mid - 1
 
-def findMin(nums: List[int]) -> int:
-    left, right = 0, len(nums)-1
-
-    result = nums[left]
-    while left <= right:
-        if nums[right] > nums[left]:
-            result = min(result, nums[left])
-            break
-        mid = (left + right) // 2
-        result = min(result, nums[mid])
-        if nums[mid] >= nums[left]:
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return result
-
-if __name__ == "__main__":
-    print(findMin([3,4,5,1,2]))
+        return result
