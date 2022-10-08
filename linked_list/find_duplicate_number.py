ec5acd5796da78a23caac38af61ea580dc12db39
ray chiu <msraychiu@gmail.com>
2022-10-08 11:22:52 +0800

    find duplicate number

---
+++ linked_list/find_duplicate_number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        s = set()
+
+        for n in nums:
+            if  n not in s:
+                s.add(n)
+            else:
+                return n
+
+        return 0
