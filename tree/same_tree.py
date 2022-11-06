c7e284581773ec8f58fe1b5c2c551a0e46e224fe
ray chiu <msraychiu@gmail.com>
2022-11-06 17:14:44 +0800

    same tree iteration on queue

---
+++ tree/same_tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        p_list = [p]
+        q_list = [q]
+
+        while p_list and q_list:
+            p_pop = p_list.pop()
+            q_pop = q_list.pop()
+
+            if (p_pop and q_pop and p_pop.val != q_pop.val) or (p_pop and not q_pop) or (not p_pop and q_pop):
+                return False
+
+            if p_pop and q_pop:
+                p_list.append(p_pop.left)
+                p_list.append(p_pop.right)
+                q_list.append(q_pop.left)
+                q_list.append(q_pop.right)
+
+        return True
