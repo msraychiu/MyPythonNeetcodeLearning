5e9d36ff3bcd12d0b613e65934284613b4586d5e
ray chiu <msraychiu@gmail.com>
2022-11-13 11:52:07 +0800

    subtree of another tree

---
+++ tree/subtree_of_another_tree.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSubtree(self, root, subRoot):
+        """
+        :type root: TreeNode
+        :type subRoot: TreeNode
+        :rtype: bool
+        """
+        q = [root]
+        while q:
+            q_pop = q.pop()
+            if self.isSameTree(q_pop, subRoot):
+                return True
+            q.append(q_pop.left)
+            q.append(q_pop.right)
+
+        return False
+
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        p_list = [p]
+        q_list = [q]
+
+        while p_list and q_list:
+            p_pop = p_list.pop()
+            q_pop = q_list.pop()
+
+            if (p_pop and q_pop and p_pop.val != q_pop.val) or (type(p_pop) != type(q_pop)):
+                return False
+
+            if p_pop and q_pop:
+                p_list.append(p_pop.left)
+                p_list.append(p_pop.right)
+                q_list.append(q_pop.left)
+                q_list.append(q_pop.right)
+
+        return True
